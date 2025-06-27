4a8bffb0edb40704b6da8bbf18ee0de2afa060b9
CAJ654 <cajgamer654@gmail.com>
2025-06-27 17:01:23 -0400

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:vob_messaging_dart_app/main.dart';
+import 'package:dart_cave_game/main.dart'; 
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
